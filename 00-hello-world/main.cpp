895a2ad3554c085fb0bb943e5e534e42836c71c6
Ernest <44341687+ernieIzde8ski@users.noreply.github.com>
2022-08-29 10:19:05 -0400

    Initial commit

---
+++ 00-hello-world/main.cpp
@@ -0,0 +1,13 @@
+/* 
+  Ernest Izdebski, 2022-08-29
+
+  This program displays "Hello World!" in the console.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
