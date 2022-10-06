f7bb7fbe0ba42b80a4eb7a0f28347587a31fb5c5
Ernest Izdebski <44341687+ernieIzde8ski@users.noreply.github.com>
2022-10-05 20:26:49 -0400

    add assignments due 07.10.2022

---
+++ 33-rock-paper-scissors/main.cpp
@@ -0,0 +1,196 @@
+/*
+  Ernest Izdebski, 2022-09-30
+
+  A program that lets the player play Rock Paper Scissors against the
+  computer. Press Q (or SIGINT) to exit.
+
+
+  at this point I'm fairly confident the homework is being compiled
+  on a -nix system, and with the extent of that confidence being
+  abused in this assignment I hope that I'm correct; at the very
+  least, it works on replit
+
+
+  while debugging, it may be useful to run:
+    for (( ; ; )) do printf '\n'; sleep 1; done | ./run 33-rock-paper-scissors/
+  to spam the Enter key into the script
+*/
+
+#include <iostream>
+#include <termios.h>
+#include <unistd.h>
+#include <vector>
+#include <map>
+
+#define elif else if
+
+using namespace std;
+
+// rock, paper, and scissor constants
+enum RockPaperScissors { rock, paper, scissors };
+const RockPaperScissors _RPS_Array[] = { rock, paper, scissors };
+RockPaperScissors random_rps() { return _RPS_Array[rand() % 3]; }
+map<RockPaperScissors, RockPaperScissors> RPS_Matchups = { {rock, scissors}, {scissors, paper}, {paper, rock} };
+string rps_names(RockPaperScissors& choice) {
+  switch (choice) {
+  case rock: return "ROCK";
+  case paper: return "PAPER";
+  case scissors: return "SCISSORS";
+  }
+}
+
+// ANSI escape sequences, use any format + ANSI_NORMAL at the end to clear formatting
+// repl.it seems to use some *nix system, so this should work fine
+string ANSI_CHAR(string code) { return "\x1b[" + code + "m"; }
+string ANSI_CHAR(int code) { return ANSI_CHAR(to_string(code)); }
+const string ANSI_NORMAL = ANSI_CHAR(0);
+const string ANSI_UNDERLINE = ANSI_CHAR(4);
+const string ANSI_FG_RED = ANSI_CHAR(31);
+const string ANSI_FG_GREEN = ANSI_CHAR(32);
+const string ANSI_FG_BLUE = ANSI_CHAR(34);
+const string ANSI_FG_MAGENTA = ANSI_CHAR(35);
+const string ANSI_BG_RED = ANSI_CHAR(41);
+const string ANSI_BG_GREEN = ANSI_CHAR(42);
+const string ANSI_BG_BLUE = ANSI_CHAR(44);
+const string ANSI_BG_MAGENTA = ANSI_CHAR(45);
+const string ANSI_BG_WHITE = ANSI_CHAR(47);
+
+
+// quick error exit
+void error(string msg, int exit_code = 1) {
+  cerr << msg << endl;
+  exit(exit_code);
+}
+
+
+// wipe console via ANSI escape code
+void clear_console() {
+  // https://paulschou.com/tools/ansi/escape.html
+  cout << "\033[2J\033[1H";
+}
+
+
+// I don't understand how this function works, but its task is
+// to get a single char from console input, without having to write newlines
+// https://stackoverflow.com/questions/3276546/how-to-implement-getch-function-of-c-in-linux
+int getch(void) {
+  struct termios oldattr, newattr;
+  int ch;
+  tcgetattr(STDIN_FILENO, &oldattr);
+  newattr = oldattr;
+  newattr.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
+  ch = getchar();
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
+  return ch;
+}
+
+struct RPS_MenuItem {
+  string name;
+  RockPaperScissors value;
+  bool operator== (RPS_MenuItem other) {
+    return (other.name == name) && (other.value == value);
+  }
+};
+
+
+// adjust contents of menu such that it displays properly
+// returns the adjusted size of inner items
+int prepare_menu(vector<RPS_MenuItem>& menu) {
+  int menu_size = menu.size();
+  if (menu_size == 0) error("error: menu must contain items");
+
+  int max_string_size = -1;
+
+  // probably not the best optimized (2 for loops) but I couldn't think of a better way
+  for (RPS_MenuItem item : menu) {
+    max_string_size = max(max_string_size, int(item.name.length()));
+  }
+
+  for (RPS_MenuItem& item : menu) {
+    item.name = "| " + item.name + string(max_string_size - item.name.length(), ' ') + " |";
+  }
+
+  return max_string_size;
+}
+
+// displays a menu with selected item
+void display_menu(vector<RPS_MenuItem> menu, string filler, int selected) {
+  cout << filler << endl;
+  string color;
+  int size = menu.size();
+  for (int i = 0; i < size; i++) {
+    color = (i == selected) ? ANSI_BG_WHITE : ANSI_BG_RED;
+    cout << color << menu[i].name << ANSI_NORMAL << endl;
+    cout << filler << endl;
+  }
+}
+
+// determine result of rock paper scissors bet
+string rps_shoot(RockPaperScissors& choice) {
+  RockPaperScissors computer_choice = random_rps();
+
+  if (RPS_Matchups[computer_choice] == choice) {
+    return ANSI_BG_MAGENTA + "SORRY,,," + ANSI_NORMAL + " COMPUTER CHOSE "
+      + ANSI_BG_GREEN + rps_names(computer_choice) + " . . . " + ANSI_NORMAL
+      + "\nYOU " + ANSI_BG_RED + "LOSE . . ." + ANSI_NORMAL;
+  } elif(RPS_Matchups[choice] == computer_choice) {
+    return ANSI_BG_MAGENTA + "HOORAH ! " + ANSI_NORMAL + "The COMPUTER CHOSE "
+      + ANSI_BG_RED + rps_names(computer_choice) + ANSI_NORMAL + " , \n"
+      + ANSI_BG_GREEN + "YOU WIN !!!!  " + ANSI_NORMAL;
+  } else return ANSI_FG_BLUE + "WHAT . . . . TRY AGAIN . . .. . .." + ANSI_NORMAL
+    + "\n" + ANSI_BG_BLUE + "(You and the Computer had the Same Result)" + ANSI_NORMAL;
+
+  return "ROLLING CODE IS UNIMPLEMENTED . . .";
+}
+
+int main() {
+  srand(time(0)); // initialize random number generator
+  vector<RPS_MenuItem> rps_menu = { {"Rock", rock}, {"Paper", paper}, {"Scissors", scissors} };
+
+  // define & populate variables
+  int rps_menu_item_size = prepare_menu(rps_menu);
+  string filler = '|' + string(rps_menu_item_size + 2, '-') + "|";
+  int selected_entry = 0; // selected item in range [0, 3)
+  const int selected_lower_bound_inclusive = 0; // selected should not go below 0
+  const int selected_upper_bound_exclusive = 3; // selected should not equal or go above 3
+  int game_no = 0;
+
+  // infinite loop (until forcibly broken)
+  string premenu_line = "At Any point , press Q to exit";
+  while (true) {
+    clear_console();
+    cout << premenu_line << endl << endl;
+    display_menu(rps_menu, filler, selected_entry);
+    cout << endl << ANSI_UNDERLINE << "Game #" << game_no << ANSI_NORMAL << endl;
+
+    int choice = getch();
+    if (choice == 'q' || choice == 'Q') break;
+
+
+    if (choice == '\n') {
+      // currently selected menu item has been chosen
+      premenu_line = rps_shoot(rps_menu[selected_entry].value);
+      game_no++;
+    } elif(choice == 27) {
+      // arrow keys send the following three strokes:
+      // 27 (Escape), 91 ('['), and one stroke in range [65, 69) ('A', 'B', 'C', 'D')
+      getch(); // ignoring the left square bracket
+      choice = getch();
+      if (choice == 65 || choice == 68) {
+        // up arrow, left arrow: move selection to above entry
+        if (selected_entry == selected_lower_bound_inclusive) // wrap entry around
+          selected_entry = selected_upper_bound_exclusive;
+        selected_entry--;
+      } elif(choice == 66 || choice == 67) {
+        // down arrow, right arrow: move selection to below entry
+        selected_entry++;
+        if (selected_entry == selected_upper_bound_exclusive)
+          selected_entry = selected_lower_bound_inclusive; // wrap entry around
+      };
+    } else // in the event of an unrecognized keypress
+      premenu_line = ANSI_BG_RED + "what" + ANSI_NORMAL;
+  }
+
+  cout << endl << ANSI_FG_MAGENTA << "Goodbye!" << ANSI_NORMAL << endl;
+}
