d65080bcd2ebaafd34e5302f56e4788b2e5e40f2
Ernest <44341687+ernieIzde8ski@users.noreply.github.com>
2022-10-21 04:19:52 -0400

    Create header files

---
+++ headers/conio.h
@@ -0,0 +1,37 @@
+// https://stackoverflow.com/questions/3276546/how-to-implement-getch-function-of-c-in-linux
+// header filename taken from its Windows counterpart
+
+// prevents the definitions in this file from being read more than once
+#pragma once
+
+#include <iostream>
+#include <termios.h>
+#include <unistd.h>
+
+namespace conio {
+/* reads from keypress, doesn't echo */
+int getch(void) {
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr(STDIN_FILENO, &oldattr);
+    newattr = oldattr;
+    newattr.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
+    return ch;
+}
+
+/* reads from keypress, echoes */
+int getche(void) {
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr(STDIN_FILENO, &oldattr);
+    newattr = oldattr;
+    newattr.c_lflag &= ~(ICANON);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
+    return ch;
+}
+}
---
+++ headers/ernie.hpp
@@ -0,0 +1,71 @@
+/*
+    ernie.hpp, a good header file
+
+    collection of functions I use to be lazy
+*/
+
+#pragma once
+
+namespace ernie {
+// simple input function
+template <typename T> T input(std::string s = "") {
+  T resp;
+  std::cout << s;
+  std::cin >> resp;
+  return resp;
+}
+
+// Array Methods
+// ALL of these are likely to segfault if an array with size 0 is
+// passed into any of them
+
+// simple array summation
+template <typename T> T arraySum(T arr[], int size) {
+  // just in case the given operator T does not support typecasts from 0
+  T resp = arr[0];
+  for (int i = 1; i < size; i++) resp += arr[i];
+  return resp;
+}
+
+// get value ranked lowest in array
+template <typename T> T arrayLowestValue(T arr[], int size) {
+  T resp = arr[0];
+  for (int i = 1; i < size; i++) resp = std::min(resp, arr[i]);
+  return resp;
+}
+
+// get index of value ranked lowest in array
+template <typename T> int arrayLowestValueIndex(T arr[], int size) {
+  int index = 0;
+  T lowest_value = arr[0];
+  for (int i = 1; i < size; i++) {
+    T next = arr[i];
+    if (next < lowest_value) {
+      index = i;
+      lowest_value = next;
+    }
+  };
+  return index;
+}
+
+// get value ranked highest in array
+template <typename T> T arrayHighestValue(T arr[], int size) {
+  T resp = arr[0];
+  for (int i = 1; i < size; i++) resp = std::max(resp, arr[i]);
+  return resp;
+}
+
+// get index of value ranked highest in array
+template <typename T> int arrayHighestValueIndex(T arr[], int size) {
+  int index = 0;
+  T highest_value = arr[0];
+  for (int i = 1; i < size; i++) {
+    T next = arr[i];
+    if (next > highest_value) {
+      index = i;
+      highest_value = next;
+    }
+  };
+  return index;
+}
+}
