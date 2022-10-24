c22ba7847f74ca484fd07834ec47ce455dd9d5cc
Ernest <44341687+ernieIzde8ski@users.noreply.github.com>
2022-10-24 13:15:06 -0400

    more ANSI abuse + macro abuse

--- headers/ansi.hpp
+++ headers/ansi.hpp
@@ -4,22 +4,95 @@
 
 #pragma once
 #include <string>
+#include <iostream>
 
 namespace ANSI {
     // https://en.wikipedia.org/wiki/ANSI_escape_code#Fe_Escape_sequences
     const std::string ESC = "\x1b";
     const std::string CSI = ESC + "[";
 
-    // basic escape sequences
-    // https://en.wikipedia.org/wiki/ANSI_escape_code#SGR_(Select_Graphic_Rendition)_parameters
-    std::string SGR(std::string code) {
-        return CSI + code + "m";
-    }
-    std::string SGR(int code) {
-        return SGR(std::to_string(code));
+    // various CSI sequences
+    // https://en.wikipedia.org/wiki/ANSI_escape_code#CSI_(Control_Sequence_Introducer)_sequences
+#define CONTROL_FUNC(NAME,VALUE,DEFAULT)    \
+void NAME(int n = DEFAULT) {                \
+    std::cout << CSI << n << VALUE;         \
+};                                                                                  
+
+
+    // Moves the cursor n (default 1) cells in the given direction.
+    // If the cursor is already at the edge of the screen, this has no effect.
+    CONTROL_FUNC(CURSOR_UP, "A", 1);
+
+    // Moves the cursor n (default 1) cells in the given direction.
+    // If the cursor is already at the edge of the screen, this has no effect.
+    CONTROL_FUNC(CURSOR_DOWN, "B", 1);
+
+    // Moves the cursor n (default 1) cells in the given direction.
+    // If the cursor is already at the edge of the screen, this has no effect.
+    CONTROL_FUNC(CURSOR_FORWARD, "C", 1);
+
+    // Moves the cursor n (default 1) cells in the given direction.
+    // If the cursor is already at the edge of the screen, this has no effect.
+    CONTROL_FUNC(CURSOR_BACK, "D", 1);
+
+    // Moves cursor to beginning of the line n (default 1) lines down.
+    CONTROL_FUNC(CURSOR_NEXT_LINE, "E", 1);
+
+    // Moves cursor to beginning of the line n (default 1) lines up.
+    CONTROL_FUNC(CURSOR_PREV_LINE, "F", 1);
+
+    // Moves the cursor to column n (default 1).
+    CONTROL_FUNC(CURSOR_HORIZONTAL_ABSOLUTE, "G", 1);
+
+    // Clears part of the screen. 
+    // - If n is 0 (or missing), clear from cursor to end of screen. 
+    // - If n is 1, clear from cursor to beginning of the screen. 
+    // - If n is 2, clear entire screen (and moves cursor to upper left on DOS ANSI.SYS).
+    // - If n is 3, clear entire screen and delete all lines saved in the scrollback buffer
+    //   (this feature was added for xterm and is supported by other terminal applications).
+    CONTROL_FUNC(ERASE_IN_DISPLAY, "J", 0);
+
+    // Erases part of the line.
+    // - If n is 0 (or missing), clear from cursor to the end of the line.
+    // - If n is 1, clear from cursor to beginning of the line.
+    // - If n is 2, clear entire line. Cursor position does not change.
+    CONTROL_FUNC(ERASE_IN_LINE, "K", 0);
+
+    // Scroll whole page up by n (default 1) lines. New lines are added at the bottom.
+    CONTROL_FUNC(SCROLL_UP, "S", 1);
+
+    // Scroll whole page down by n (default 1) lines. New lines are added at the top.
+    CONTROL_FUNC(SCROLL_DOWN, "T", 1);
+
+    // some control functions disobey the 1-param rule
+
+    // Moves the cursor to row n, column m. The values are 1-based,
+    // and default to 1 (top left corner) if omitted.
+    void CURSOR_POSITION(int n = 1, int m = 1) {
+        std::cout << CSI << n << ";" << m << "H";
     }
 
-    /* 
+    // ???
+    // "Same as CUP, but counts as a format effector function (like
+    // CR or LF) rather than an editor function (like CUD or CNL).
+    // This can lead to different handling in certain terminal modes."
+    void HORIZONTAL_VERTICAL_POSITION(int n = 1, int m = 1) {
+        std::cout << CSI << n << ";" << m << "f";
+    }
+
+#undef CONTROL_FUNC
+
+
+    // specific CSI case, returns the formatted string
+    // https://en.wikipedia.org/wiki/ANSI_escape_code#SGR_(Select_Graphic_Rendition)_parameters
+    std::string SGR(std::string code = "0") {
+        return CSI + code + "m";
+    }
+    std::string SGR(int code = 0) {
+        return CSI + std::to_string(code) + "m";
+    }
+
+    /*
     colors from the predefined set of 256 colors
     https://en.wikipedia.org/wiki/ANSI_escape_code#8-bit
         0-  7:  standard colors (as in ESC [ 30–37 m)
@@ -27,49 +100,53 @@ namespace ANSI {
         16-231:  6 × 6 × 6 cube (216 colors): 16 + 36 × r + 6 × g + b (0 ≤ r, g, b ≤ 5)
         232-255:  grayscale from dark to light in 24 steps
     */
-    std::string color256(std::string code, bool fg = true) {
+    std::string SGR_256(std::string code, bool fg = true) {
         return CSI + (fg ? "38:5:" : "48:5:") + code + "m";
     }
-    std::string color256(int code, bool fg = true) {
-        return color256(std::to_string(code), fg);
+    std::string SGR_256(int code, bool fg = true) {
+        return SGR_256(std::to_string(code), fg);
     }
 
     // consoles such as KDE's Konsole & GNOME Terminal support full 24-bit RGB
-    std::string colorRGB(std::string red, std::string green, std::string blue, bool fg = true) {
+    std::string SGR_RGB(std::string red, std::string green, std::string blue, bool fg = true) {
         return CSI + (fg ? "38;" : "48;") + "2;" + red + ";" + green + ";" + blue + "m";
     }
-    std::string colorRGB(int red, int green, int blue, bool fg = true) {
-        return colorRGB(
+    std::string SGR_RGB(int red, int green, int blue, bool fg = true) {
+        return SGR_RGB(
             std::to_string(red), std::to_string(green), std::to_string(blue), fg
         );
     }
 
-    // misc codes - see above wikipeda url
-    const std::string NORMAL = SGR(0);
+#define CODE(NAME,VALUE) const std::string NAME = SGR( VALUE )
+
+    // misc codes - see above wikipedia url
+    CODE(NORMAL, 0);
+    CODE(BOLD, 1);
+    CODE(DIM, 2);
+    CODE(ITALIC, 3);
+    CODE(UNDERLINE, 4);
+    CODE(STRIKETHROUGH, 9);
     const std::string RESET = NORMAL;
-    const std::string BOLD = SGR(1);
-    const std::string DIM = SGR(2);
-    const std::string ITALIC = SGR(3);
-    const std::string UNDERLINE = SGR(4);
-    const std::string STRIKETHROUGH = SGR(9);
 
     // basic foreground colors
-    const std::string FG_BLACK = SGR(30);
-    const std::string FG_RED = SGR(31);
-    const std::string FG_GREEN = SGR(32);
-    const std::string FG_YELLOW = SGR(33);
-    const std::string FG_BLUE = SGR(34);
-    const std::string FG_MAGENTA = SGR(35);
-    const std::string FG_CYAN = SGR(36);
-    const std::string FG_WHITE = SGR(37);
+    CODE(FG_BLACK, 30);
+    CODE(FG_RED, 31);
+    CODE(FG_GREEN, 32);
+    CODE(FG_YELLOW, 33);
+    CODE(FG_BLUE, 34);
+    CODE(FG_MAGENTA, 35);
+    CODE(FG_CYAN, 36);
+    CODE(FG_WHITE, 37);
 
     // basic background colors
-    const std::string BG_BLACK = SGR(40);
-    const std::string BG_RED = SGR(41);
-    const std::string BG_GREEN = SGR(42);
-    const std::string BG_YELLOW = SGR(43);
-    const std::string BG_BLUE = SGR(44);
-    const std::string BG_MAGENTA = SGR(45);
-    const std::string BG_CYAN = SGR(46);
-    const std::string BG_WHITE = SGR(47);
+    CODE(BG_BLACK, 40);
+    CODE(BG_RED, 41);
+    CODE(BG_GREEN, 42);
+    CODE(BG_YELLOW, 43);
+    CODE(BG_BLUE, 44);
+    CODE(BG_MAGENTA, 45);
+    CODE(BG_CYAN, 46);
+    CODE(BG_WHITE, 47);
+
+#undef CODE
 }
--- n-01-test-colors/main.cpp
+++ n-01-test-colors/main.cpp
@@ -8,7 +8,7 @@
 #include <iostream>
 
 int main() {
-  ANSI::colorRGB(0, 0, 0);
+  ANSI::SGR_RGB(0, 0, 0);
   char text[13] = "Hello World!";
   std::string codes[13] = {
     ANSI::BG_BLACK,
@@ -19,16 +19,20 @@ int main() {
     ANSI::BG_RED,
     ANSI::FG_CYAN,
     ANSI::ITALIC,
-    ANSI::colorRGB(0, 128, 64),
+    ANSI::SGR_RGB(0, 128, 64),
     ANSI::BG_MAGENTA,
     ANSI::FG_GREEN,
     ANSI::FG_RED,
     ANSI::BG_WHITE,
   };
+  ANSI::ERASE_IN_DISPLAY(2);
+  ANSI::CURSOR_POSITION(4);
   for (int i = 0; i < 13; i++) {
     std::cout << codes[i] << text[i] << ANSI::NORMAL;
   }
   std::cout << std::endl;
-
-  std::cout << ANSI::BOLD << "The demonstration has concluded.\n" << ANSI::NORMAL << std::endl;
+  ANSI::CURSOR_POSITION();
+  std::cout << ANSI::BOLD << "The demonstration has concluded." << ANSI::NORMAL << std::endl;
+  std::cout << "(Went backwards Lol)" << std::endl;
+  ANSI::CURSOR_POSITION(6);
 }
