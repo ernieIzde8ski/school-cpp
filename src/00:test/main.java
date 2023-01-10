727d22bbdb55816cc54de02b2b086585db64880e
Ernest <44341687+ernieIzde8ski@users.noreply.github.com>
2023-01-10 12:28:40 -0500

    add mknext script

---
+++ src/00:test/main.java
@@ -0,0 +1,13 @@
+/*
+    Ernest Izdebski
+    2023-01-10
+
+    A test of the mknext script.
+*/
+
+
+class test {
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+}
