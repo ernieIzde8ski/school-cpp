7f842071e79d8fb7d63ae8f082e1e03c98157d47
Ernest <44341687+ernieIzde8ski@users.noreply.github.com>
2023-01-11 12:05:06 -0500

    update mknext script to use classname more

--- src/00:test/Test.java
+++ src/00:test/Test.java
@@ -1,12 +1,12 @@
 /*
     Ernest Izdebski
-    2023-01-10
+    2023-01-11
 
     A test of the mknext script.
 */
 
 
-class test {
+public class Test {
     public static void main(String[] args) {
         System.out.println("Hello, world");
     }
